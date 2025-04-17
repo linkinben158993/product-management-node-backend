3d86042bacd1578622f91a252892e11cbc756bf8
linkinben158993 <thienan.nguyenhoang311@gmail.com>
2025-04-18 00:11:09 +0700

    Basic CRUD for users entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.usersRepository.save(createUserDto);
+    console.log('Newly created user', user);
+    return user;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    const users = await this.usersRepository.find();
+    console.log('Found users', users);
+    return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    console.log('Found user', user);
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const foundUser = await this.findOne(id);
+    if (!foundUser) {
+      throw new NotFoundException();
+    }
+
+    Object.assign(foundUser, updateUserDto);
+    console.log(
+      'Update found user',
+      foundUser,
+      'with information',
+      updateUserDto,
+    );
+
+    return this.usersRepository.save(foundUser);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    const foundUser = await this.findOne(id);
+    if (!foundUser) {
+      throw new NotFoundException();
+    }
+
+    console.log('Removing user with id', id);
+
+    return await this.usersRepository.remove(foundUser);
   }
 }
